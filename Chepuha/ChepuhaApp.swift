e55a3dde43cc38aae3888858c1dc5104f22dec3c
LofoD <evkaverin@gmail.com>
2021-05-20 14:56:55 +0300

    Initial Commit

---
+++ Chepuha/ChepuhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChepuhaApp.swift
+//  Chepuha
+//
+//  Created by LofoD on 20.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChepuhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chepuha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chepuha
+//
+//  Created by LofoD on 20.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
